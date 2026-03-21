22025a18d4a3e1698c04ae3b2fe8d1d620c3d131
toeneeoh <toeneeoh@gmail.com>
2026-03-21 12:09:29 -0400

    remove Location instances

--- cotlua/src/Game/timers.lua
+++ cotlua/src/Game/timers.lua
@@ -116,7 +116,7 @@ OnInit.final("Timers", function(Require)
 
     local function OneSecond()
         -- set space bar camera to town
-        SetCameraQuickPositionLoc(TOWN_CENTER)
+        SetCameraQuickPosition(TOWN_CENTER_X, TOWN_CENTER_Y)
 
         -- fountain regeneration
         ALICE_ForAllObjectsInRangeDo(fountain, -260., 350., 600., "unit")
